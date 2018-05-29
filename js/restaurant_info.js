bf7847ee8f6afab0bc7a4a7a90f6a173efafe68e
Erin Witmer <ecwitmer@gmail.com>
2018-05-29 15:06:27 -0400

    feat: Add alt text to images

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -56,8 +56,11 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   address.innerHTML = restaurant.address;
 
   const image = document.getElementById('restaurant-img');
-  image.className = 'restaurant-img'
+  image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  console.log(restaurant)
+  image.alt = restaurant.photo;
+
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
